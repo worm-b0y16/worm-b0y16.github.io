e52ac72c1ba7d6ae6d2d26dce3cf54d7b00b9a20
worm-b0y16 <147778016+worm-b0y16@users.noreply.github.com>
2024-02-26 12:08:09 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -38,7 +38,7 @@ function getRandomIndex(max) {
 
 async function populate() {
     const requestURL = 
-      "https://github.com/worm-b0y16/worm-b0y16.github.io/blob/main/scripts/dogImageData.json";
+      "https://github.com/worm-b0y16/worm-b0y16.github.io/blob/30be28972024d6fa19c9d7d786ed093266e34fbc/scripts/dogImageData.json";
     const request = new Request(requestURL);
     const response = await fetch(request);
     imageData = await response.json();
