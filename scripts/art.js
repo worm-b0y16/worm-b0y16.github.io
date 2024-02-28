fec174cfb9b3b1cc5640cbd76ec625eef934401d
worm-b0y16 <worm16@protonmail.com>
2024-02-28 13:59:21 -0500

    Update art.js

--- scripts/art.js
+++ scripts/art.js
@@ -17,7 +17,7 @@ sortButton[4].addEventListener("click", otherSort);
 function twentyThreeSort() {
     var temp;
     for(i = 0; i < (imageData["art"].length); i++) {
-        if ((imageData."art"[i]."tags"["year"]) == 23) {
+        if ((imageData["art"[i]]."tags"["year"]) == 23) {
             temp = imageData["art"[i]].index;
             temp = newLength = twentyThreeList.push(temp);
         }
